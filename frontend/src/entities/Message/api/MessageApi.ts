a39c1ef251f23ce365d258b7aadb669726bbc3e3
VildanJS <habvil@yandex.ru>
2024-02-13 17:09:34 +0500

    update api for v2

---
+++ frontend/src/entities/Message/api/MessageApi.ts
@@ -0,0 +1,83 @@
+import { rtkApi } from '@/shared/api/rtkApi'
+import { createSelector } from '@reduxjs/toolkit'
+import { io } from 'socket.io-client'
+
+export interface MessageType {
+    body: string,
+    channelId: string;
+    username: string;
+    id: string;
+}
+
+export const messageApi = rtkApi.enhanceEndpoints({ addTagTypes: ['Message'] }).injectEndpoints({
+        endpoints: (builder) => ({
+            getMessages: builder.query<MessageType[], void>({
+                query: () => '/messages',
+                async onCacheEntryAdded(
+                    arg,
+                    { cacheDataLoaded, updateCachedData },
+                ) {
+                    const socket = io()
+                    socket.on('newMessage', (data) => {
+                        updateCachedData((draft) => {
+                            draft.push(data)
+                        })
+                    })
+                    await cacheDataLoaded
+                    // socket.off('newMessage')
+                },
+                providesTags: (result = []) => {
+                    const messagesWithId = result.map(({ id }) => ({ type: 'Message' as const, id }))
+                    return result
+                        ? [{ type: 'Message', id: 'LIST' }, ...messagesWithId]
+                        : [{ type: 'Message', id: 'LIST' }]
+                },
+            }),
+            addNewMessage: builder.mutation<MessageType, { body: string, channelId: string, username: string }>({
+                query: ({ body, channelId, username }) => ({
+                    url: '/messages',
+                    method: 'POST',
+                    body: { body, channelId, username },
+                }),
+                invalidatesTags: [{ type: 'Message', id: 'LIST' }],
+            }),
+            editMessage: builder.mutation<MessageType, { body: string, messageId: string }>({
+                query: ({ body, messageId }) => ({
+                    url: `/messages/${messageId}`,
+                    method: 'PATCH',
+                    body: { body },
+                    params: {messageId}
+                }),
+                invalidatesTags: (result, error, arg) => [{ type: 'Message', id: arg.messageId }],
+            }),
+            deleteMessage: builder.mutation<{ messageId: string }, { messageId: string }>({
+                query: ({ messageId }) => ({
+                    url: `/channels/${messageId}`,
+                    method: 'DELETE',
+                    body: { messageId },
+                }),
+                invalidatesTags: (result, error, arg) => [{ type: 'Message', id: arg.messageId }],
+            }),
+        }),
+    },
+)
+
+const selectMessagesResult = messageApi.endpoints.getMessages.select()
+const selectAllMessages = createSelector(
+    selectMessagesResult,
+    channelsResult => channelsResult.data ?? [],
+)
+export const selectMessagesCountByChannelId = createSelector(
+    selectAllMessages,
+    (messages, channelId, ) => channelId,
+    (messages, channelId) => {
+        return  messages
+            .filter((message: MessageType) => message.channelId === channelId)
+            .length
+    }
+)
+
+export const {
+    useGetMessagesQuery,
+    useAddNewMessageMutation
+} = messageApi
